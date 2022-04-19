7fefd5d1ed99ee9736a411ef6477e106a55b4fbc
Reza kazemi <rezakazemi890@gmail.com>
2022-04-19 18:18:35 +0430

    first commit

---
+++ src/Equinox.Services.Api/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Sentry;
+
+namespace Equinox.Services.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+
+            //SentrySdk.CaptureMessage("Hello Sentry");
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSentry(o =>
+                    {
+                        o.Dsn = "https://8ea104c743b24408ae25d0044905aa31@o1207501.ingest.sentry.io/6340936";
+                        // When configuring for the first time, to see what the SDK is doing:
+                        o.Debug = true;
+                        // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+                        // We recommend adjusting this value in production.
+                        o.TracesSampleRate = 1.0;
+                    });
+                });
+    }
+}
